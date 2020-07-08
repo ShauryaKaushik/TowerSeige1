69e24ccafbf454ca7b8bad16b3b756548b2312df
ShauryaKaushik <65485931+ShauryaKaushik@users.noreply.github.com>
2020-07-08 18:04:37 +0530

    Update Enemy.js

--- Enemy.js
+++ Enemy.js
@@ -5,7 +5,7 @@ class Enemy {
           'friction':1.0,
           'density':1.0
       }
-      this.body = Bodies.rectangle(x, y,50,50,options);
+      this.body = Bodies.rectangle(x, y,100,100,options);
       this.width = 50;
       this.height = 50;
       this.image = loadImage("lavaBall.png");
